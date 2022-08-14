ac165638d4c9723e289144fc5e195153f43eb78f
Luiz Carlos Silvestrini <luizsilvestrini15@hotmail.com>
2022-08-14 00:09:59 -0300

    Custom buttonIcon

--- frontweb/src/components/buttonIcon/index.tsx
+++ frontweb/src/components/buttonIcon/index.tsx
@@ -1,14 +1,18 @@
 import './styles.css';
+import { ReactComponent as ArrowIcon } from 'assets/image/seta.svg';
 
-const ButtonIcon =() =>{
+const ButtonIcon = () => {
+  return (
+    <div className="btn-container">
+      <button className="btn btn-primary">
+        <h6>INICIE AGORA A SUA BUSCA</h6>
+      </button>
 
-    return(
-        <button className="btn btn-primary btn-icon">
-            <h6>INICIE AGORA A SUA BUSCA</h6>
-        </button>
-    );
-
-    
-}
+      <div className="btn-icon-container">
+        <ArrowIcon />
+      </div>
+    </div>
+  );
+};
 
 export default ButtonIcon;
